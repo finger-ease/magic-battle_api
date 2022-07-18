a3fc224de895f009b3aa2920862b00b362e29d3c
finger-ease <fingerEase24@gmail.com>
2022-07-18 18:49:04 +0900

    update: 環境変数が設定されていない場合のポート番号を設定

--- server.ts
+++ server.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import express from 'express';
 import { getBattler } from './src/api/getBattler';
 
+const PORT = process.env.PORT || 3000;
+
 const server = express();
 server.get('/:name', (req, res) => {
   res.status(200).json(getBattler(req.params.name));
 });
 
-server.listen(process.env.PORT);
+server.listen(PORT);
