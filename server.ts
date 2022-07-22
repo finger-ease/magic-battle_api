c7367164528a81ee87b2cad3069016fa0bcb92de
finger-ease <fingerEase24@gmail.com>
2022-07-22 20:14:18 +0900

    update: ルーティングを変更

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { getBattler } from './src/api/getBattler';
 const PORT = process.env.PORT || 3000;
 
 const server = express();
-server.get('/:name', (req, res) => {
+server.get('/battler/:name', (req, res) => {
   res.status(200).json(getBattler(req.params.name));
 });
 
